019b7fcef9925c4b98ee6d9f7dfdcc518178bc01
U_sang <usang6325@gmail.com>
2018-04-09 17:37:48 +0900

    db에 저장된 script config 받아서 job_file을 생성 후 실행하는 기능

--- ClientServer/app.js
+++ ClientServer/app.js
@@ -6,44 +6,115 @@ var exec = require('child_process').exec;
 var async = require('async');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.set('views', './views');
 app.set('view engine', 'jade');
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
   res.render('view');
 });
 
-app.post('/', function(req, res){
-  var name = req.body.script_name;
+app.post('/', function(req, res) {
+  var name = req.body.name;
+  var read = JSON.parse(req.body.read);
+  var write = JSON.parse(req.body.write);
+  var rand = JSON.parse(req.body.rand);
+
+  var readwrite_arr = new Array(2);
+  readwrite_arr[0] = new Array(2);
+  readwrite_arr[1] = new Array(2);
+  readwrite_arr[0][0] = new Array(2);
+  readwrite_arr[0][1] = new Array(2);
+  readwrite_arr[1][0] = new Array(2);
+  readwrite_arr[1][1] = new Array(2);
+
+  readwrite_arr[0][0][0] = "error";
+  readwrite_arr[0][0][1] = "error";
+  readwrite_arr[0][1][0] = "swqr";
+  readwrite_arr[0][1][1] = "randw";
+  readwrite_arr[1][0][0] = "seqr";
+  readwrite_arr[1][0][1] = "randr";
+  readwrite_arr[1][1][0] = "seqrw";
+  readwrite_arr[1][1][1] = "randrw";
+
   console.log(req.body);
-  var tasks = [
-    function(callback){
-      fs.writeFile('/root/fio_Test_xlxs/controller/exec-list', name+'\n', function(err){
-        if(err){
-          console.log(err);
+
+  async.series([
+
+      function(callback) {
+        var script_form =
+        "ioengine=libaio \n direct=1 \n numjobs=1 \n group_reporting=1 \n time_based=1 \n invalidate=1 \n norandommap=1 \n randrepeat=0 \n log_avg_msec=1000 \n  write_bw_log=./tmp/bw \n write_iops_log=./tmp/iops \n write_lat_log=./tmp/lat \n" +
+        "[fio_job] \n filename=/dev/nvme0n1 \n #filename=/dev/md0";
+
+        var config = "[global] \n readwrite=" + readwrite_arr[read][write][rand] + "\n";
+        for(var i in req.body){
+          if(i == 'name' || i == 'read' || i == 'write' || i == 'rand'){
+            continue;
+          }
+          else{
+            config += i+ "=" + req.body[i] +"\n";
+          }
         }
-        console.log("Write exec-list.txt Success");
-      });
-      callback(null, "write");
-    },
-    function(callback){
-      exec('cd /root/fio_Test_xlxs/controller; sh do-test', function(err, stdout, stderr){
-        console.log(stdout);
-        console.log(stderr);
-        if(err != null){
-          console.log("error: " + err);
+
+        fs.writeFile('/root/fio_script/'+name, config+script_form, 'utf-8', function(err){
+          if(err){
+            console.log(err);
+          }
+          console.log("write script success");
+          callback(null,'0');
+        });
+      },
+
+      function(callback) {
+        fs.appendFile('/root/fio_script/run.sh', "fio "+ name + '\n', function(err) {
+          if (err) {
+            console.log(err);
+          }
+          console.log("Write script.txt Success");
+          callback(null, '1');
+        });
+      },
+
+      function(callback) {
+        exec('cd /root/fio_script; fio '+name, function(err, stdout, stderr) {
+          console.log(stdout);
+          console.log(stderr);
+          if (err != null) {
+            console.log("error: " + err);
+          }
+          console.log("Exec test Success");
+          callback(null, '2');
+        });
+      },
+
+      function(callback){
+        var logList = new Array('bw_bw.1.log', 'iops_iops.1.log', 'lat_clat.1.log', 'lat_lat.1.log', 'lat_slat.1.log');
+        var jsonList = new Array(5);
+
+        for(var i=0; i<logList.length; i++){
+          var data = fs.readFileSync('/root/fio_script/tmp/'+logList[i], 'utf-8');
+          var arr = data.split('\n');
+
+          for(var j=0; j<arr.length-1; j++){
+            arr[j] = arr[j].split(',');
+          }
+          jsonList[i] = arr;
         }
-        res.send(stdout);
-      });
-      callback(null, "stdout");
+        res.json(jsonList);
+        console.log("json success");
+      }
+    ],
+
+    function(err, results) {
+      if (err) {
+        console.log(err);
+      }
     }
-  ];
-  async.series(tasks, function(err, results){
-    console.log(results);
-  });
+  );
 });
 
-app.listen(3000, function(){
+app.listen(3000, function() {
   console.log("Connected Test Server(port 3000)");
 });
